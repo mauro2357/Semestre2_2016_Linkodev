de855a78c108f0bc05eee35d10ba40e4fb60ab3d
mahc36 <matteohenao37@hotmail.com>
2016-11-04 15:13:37 -0500

    nada

--- src/java/SERVICIOS/ApplicationConfig.java
+++ src/java/SERVICIOS/ApplicationConfig.java
@@ -29,8 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(SERVICIOS.ServicioAutenticacion.class);
-        resources.add(SERVICIOS.path.class);
     }
     
 }
